1c9af3fc7788a8043c21e7fbaa39a487677c74a4
HALWAAAAA <taras.kotsyuba2003@gmail.com>
2024-11-20 15:55:22 +0100

    Initial commit

---
+++ src/components/Balance.vue
@@ -0,0 +1,15 @@
+<template>
+    <h4>Your Balance</h4>
+    <h1 id="balance">zł{{total}}</h1>
+
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+const props = defineProps({
+    total:{
+        type: Number,
+        required: true
+    }
+});
+</script>
